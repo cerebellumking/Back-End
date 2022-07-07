8d1bdc389139f471a9b86b9fda108a86ef6c406c
cerebellumking <1805219550@qq.com>
2022-07-07 21:22:49 +0800

    查看answerinfo api、完善userinfo api返回json数据格式

---
+++ Controllers/AnswerController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Back_End.Models;
+namespace Back_End.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnswerController : ControllerBase
+    {
+        private readonly ModelContext myContext;
+        public AnswerController(ModelContext modelContext)
+        {
+            myContext = modelContext;
+        }
+        
+        [HttpGet]
+        public string getAnswerInfo(int answer_id)
+        {
+            Message message = new Message();
+            try
+            {
+                Answer answer = myContext.Answers.Single(b => b.AnswerId == answer_id);
+                message.data.Add("answer_user_id", answer.AnswerUserId);
+                message.data.Add("question_id", answer.QuestionId);
+                message.data.Add("answer_date", answer.AnswerDate);
+                message.data.Add("answer_content", answer.AnswerContent);
+                message.data.Add("answer_coin", answer.AnswerCoin);
+                message.data.Add("answer_like", answer.AnswerLike);
+                message.status = true;
+                message.errorCode = 200;
+            }
+            catch
+            {
+
+            }
+            return message.ReturnJson();
+        }
+    }
+}
--- Controllers/UserInfoController.cs
+++ Controllers/UserInfoController.cs
@@ -33,20 +33,21 @@ namespace Back_End.Controllers
             Message message = new Message();
             try
             {
+                //查询有学历认证的用户
                 var qualificationList = myContext.Qualifications.Where(a=>a.UserId==user_id&&a.Visible==true).Select(b => new { b.UserId, b.Identity, b.UniversityId }).ToList();
-                int count = 1;
                 message.data.Add("user_id", user_id);
+                List<IdentityInfo> infos=new List<IdentityInfo>();
+                //遍历不同identityID，将用户信息保存到identityInfo后存到infos中
                 foreach (var qualification in qualificationList)
                 {
                     IdentityInfo identityInfo = new IdentityInfo();
-                    //identityInfo.user_id = qualification.UserId;
                     identityInfo.identity = qualification.Identity;
                     University university = myContext.Universities.Single(b => b.UniversityId == qualification.UniversityId);
                     identityInfo.university_name = university.UniversityName;
-                    message.data.Add("identity" + count.ToString(), identityInfo);
-                    count++;
+                    infos.Add(identityInfo);
                 }
-                //Console.WriteLine(count);
+                //以数组形式返回
+                message.data.Add("identity_info",infos.ToArray());
                 message.status = true;
                 message.errorCode = 200;
             }
