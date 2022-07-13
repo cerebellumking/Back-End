3eacea1a0c0ff57bb1b1465b0213768b5c3eb33f
cerebellumking <1805219550@qq.com>
2022-07-13 16:27:20 +0800

    发布评论功能

--- Controllers/AnswerController.cs
+++ Controllers/AnswerController.cs
@@ -97,5 +97,52 @@ namespace Back_End.Controllers
             return message.ReturnJson();
         }
 
+        [HttpPost("comment")]
+        public string sendComment(int answer_id, int answer_comment_user_id, string answer_comment_content)
+        {
+            Message message = new Message();
+            try
+            {
+                myContext.DetachAll();
+                answer_id = int.Parse(Request.Form["answer_id"]);
+                answer_comment_user_id = int.Parse(Request.Form["answer_comment_user_id"]);
+                answer_comment_content = Request.Form["answer_comment_content"];
+                Answer answer = myContext.Answers.Single(b => b.AnswerId == answer_id);
+                User user = myContext.Users.Single(b => b.UserId == answer_comment_user_id);
+                int id = myContext.Answercomments.Count() + 1;
+                Answercomment answercomment = new Answercomment();
+                answercomment.AnswerCommentUser = user;
+                answercomment.AnswerCommentVisible = true;
+                answercomment.AnswerCommentReply = null;
+                answercomment.AnswerCommentFather = answer_id;
+                answercomment.AnswerCommentContent = answer_comment_content;
+                answercomment.AnswerCommentFatherNavigation = answer;
+                answercomment.AnswerCommentId = id;
+                answercomment.AnswerCommentTime = DateTime.Now;
+                answercomment.AnswerCommentUserId = answer_comment_user_id;
+                
+                Answercommentreport answercommentreport = new Answercommentreport();
+                answercommentreport.AdministratorId = 1;
+                answercommentreport.Administrator = myContext.Administrators.Single(b => b.AdministratorId == 1);
+                answercommentreport.AnswerCommentId = id;
+                answercommentreport.AnswerComment = answercomment;
+                answercommentreport.ReportId = myContext.Answercommentreports.Count() + 1;
+                answercommentreport.User = user;
+                answercommentreport.UserId = answer_comment_user_id;
+
+                myContext.Answercommentreports.Add(answercommentreport);
+                myContext.Answercomments.Add(answercomment);
+                myContext.SaveChanges();
+                message.data.Add("answer_comment_id", id);
+                message.status = true;
+                message.errorCode = 200;
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return message.ReturnJson();
+        }
     }
 }
