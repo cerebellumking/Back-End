13fd468b216f751b70d7dc5bc292d3c8e28d9aea
cerebellumking <1805219550@qq.com>
2022-07-27 15:17:56 +0800

    添加验证码功能

--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -5,13 +5,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Back_End.Models;
-
+using Microsoft.Extensions.Caching.Memory;
 namespace Back_End.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class RegisterController : ControllerBase
     {
+        public static IMemoryCache _cache = new MemoryCache(new MemoryCacheOptions());
+
         private readonly ModelContext myContext;
         public RegisterController(ModelContext modelContext)
         {
@@ -26,21 +28,32 @@ namespace Back_End.Controllers
             {
                 string user_phone = front_end_data.GetProperty("user_phone").ToString();
                 string user_password = front_end_data.GetProperty("user_password").ToString();
-
-                myContext.DetachAll();
-                User user = new User();
-                user.UserGender = "m";
-                user.UserName = "用户" + user_phone;
-                user.UserPhone = user_phone;
-                user.UserPassword = user_password;
-                user.UserCreatetime = DateTime.Now;
-                var count = myContext.Users.Count();
-                int id= myContext.Users.Count() + 1;
-                user.UserId = id;
-                myContext.Users.Add(user);
-                myContext.SaveChanges();
-                message.status = true;
-                message.data["user_id"] = id;
+                int code = int.Parse(front_end_data.GetProperty("code").ToString());
+                if (_cache.TryGetValue(user_phone, out code))
+                {
+                    _cache.Remove(user_phone);
+                    myContext.DetachAll();
+                    User user = new User();
+                    user.UserGender = "m";
+                    user.UserName = "用户" + user_phone;
+                    user.UserPhone = user_phone;
+                    user.UserPassword = user_password;
+                    user.UserCreatetime = DateTime.Now;
+                    var count = myContext.Users.Count();
+                    int id = myContext.Users.Count() + 1;
+                    user.UserId = id;
+                    myContext.Users.Add(user);
+                    myContext.SaveChanges();
+                    message.status = true;
+                    message.errorCode = 200;
+                    message.data["user_id"] = id;
+                }
+                else
+                {
+                    message.errorCode = 200;
+                    message.status = false;
+                    message.data["error"] = "验证码有误或验证超时";
+                }
             }
             catch (Exception e)
             {
@@ -49,10 +62,45 @@ namespace Back_End.Controllers
             return message.ReturnJson();
         }
 
-        [HttpPost("verifyCode")]
-        public string sendVerifyCode()
+        [HttpPost("verifycode")]
+        public string sendVerifyCode(dynamic front_end_data)
         {
-            return "成功";
+            Message message = new();
+            try
+            {
+                string user_phone = front_end_data.GetProperty("user_phone").ToString();
+                Random random = new Random();
+                int code_ = random.Next(100000, 999999);
+                _cache.Set(user_phone, code_, new TimeSpan(0, 0, 180));
+                var client = CreateClient("LTAI5tL73LUQtWCbdwGZR2gp", "hTGTKeWp0cKJhtHgsl9dg93ncY1LpQ");
+                AlibabaCloud.SDK.Dysmsapi20170525.Models.SendSmsRequest sendReq = new AlibabaCloud.SDK.Dysmsapi20170525.Models.SendSmsRequest
+                {
+                    PhoneNumbers = user_phone,
+                    SignName = "候鸟留学",
+                    TemplateCode = "SMS_244555845",
+                    TemplateParam = "{\"code\":\"" + code_.ToString() + "\"}"
+                };
+                AlibabaCloud.SDK.Dysmsapi20170525.Models.SendSmsResponse sendResp = client.SendSms(sendReq);
+                string code = sendResp.Body.Code;
+                if (code == "OK")
+                {
+                    message.errorCode = 200;
+                    message.status = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return message.ReturnJson();
+        }
+
+        public static AlibabaCloud.SDK.Dysmsapi20170525.Client CreateClient(string accessKeyId, string accessKeySecret)
+        {
+            AlibabaCloud.OpenApiClient.Models.Config config = new AlibabaCloud.OpenApiClient.Models.Config();
+            config.AccessKeyId = accessKeyId;
+            config.AccessKeySecret = accessKeySecret;
+            return new AlibabaCloud.SDK.Dysmsapi20170525.Client(config);
         }
 
     }
